2b5eeaa0c6192d9a98e4c5a3e688e2516c2e9e4e
Wilmar-Smit <u24584216@tuks.co.za>
2026-08-03 11:11:32 +0200

    Update bookingValidator.java

--- src/validators/bookingValidator.java
+++ src/validators/bookingValidator.java
@@ -1,10 +1,32 @@
 package validators;
 
+import entities.Booking;
 import entities.Equipment;
+import entities.Researcher;
 
 public class bookingValidator {
 
     boolean ValidateOS(Equipment eq) {
         return eq.getStatus() == "Out of Service";
+    }
+
+    boolean ValidateTimes(String start, String end) {
+        return TimeValidator.ValidateOrder(start, end);
+    }
+
+    boolean researcherNumBookings(Researcher researcher) {
+        // does backend lookup
+        return false;
+    }
+
+    // searches for all bookings with this eq and checks theres no conflicting time slot
+    // for the date
+    boolean researcherConflictBooking(
+        Equipment eq,
+        String date,
+        String start,
+        String end
+    ) {
+        return false;
     }
 }
